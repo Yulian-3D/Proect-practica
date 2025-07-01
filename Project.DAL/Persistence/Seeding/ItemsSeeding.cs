8c8f739457374198b73e34d28d3d073e001314e4
Юліан Гудан <iulicgudan22@gmail.com>
2025-07-02 00:52:04 +0300

    левиафан

--- Project.DAL/Persistence/Seeding/ItemsSeeding.cs
+++ Project.DAL/Persistence/Seeding/ItemsSeeding.cs
@@ -24,13 +24,239 @@ namespace Project.DAL.Persistence.Seeding
         private static void SeedItemEntities()
         {
             Items.Add(new Item()
-            {
+            {//////УЛЬДУАР//////
+                //Огненный Левиафан
                 Id = 1,
-                Name = "Dagger of Darkness",
-                Price = 51,
+                Name = "Боевой шлем в железных заклепках",
+                Price = 75000,
                 PhotoLink = "",
                 Rarity = RarityEnum.Epic,
-                Description = ""
+                Description = "голова"
+            });
+            Items.Add(new Item()
+            {
+                Id = 2,
+                Name = "Восходящее солнце",
+                Price = 75000,
+                PhotoLink = "",
+                Rarity = RarityEnum.Epic,
+                Description = "метательное"
+            });
+            Items.Add(new Item()
+            {
+                Id = 3,
+                Name = "Золотистый саронитовый коготь",
+                Price = 150000,
+                PhotoLink = "",
+                Rarity = RarityEnum.Epic,
+                Description = "правая рука"
+            });
+            Items.Add(new Item()
+            {
+                Id = 4,
+                Name = "Катушка Левиафана",
+                Price = 75000,
+                PhotoLink = "",
+                Rarity = RarityEnum.Epic,
+                Description = "кольцо"
+            });
+            Items.Add(new Item()
+            {
+                Id = 5,
+                Name = "Колье-оберег Фрейи",
+                Price = 75000,
+                PhotoLink = "",
+                Rarity = RarityEnum.Epic,
+                Description = "шея"
+            });
+            Items.Add(new Item()
+            {
+                Id = 6,
+                Name = "Мощь автоматизации",
+                Price = 75000,
+                PhotoLink = "",
+                Rarity = RarityEnum.Epic,
+                Description = "кольцо"
+            });
+            Items.Add(new Item()
+            {
+                Id = 7,
+                Name = "Наплечные пластины дремлющей энергии",
+                Price = 75000,
+                PhotoLink = "",
+                Rarity = RarityEnum.Epic,
+                Description = "плечо"
+            });
+            Items.Add(new Item()
+            {
+                Id = 8,
+                Name = "Наручники механика",
+                Price = 75000,
+                PhotoLink = "",
+                Rarity = RarityEnum.Epic,
+                Description = "наручи"
+            });
+            Items.Add(new Item()
+            {
+                Id = 9,
+                Name = "Облачение Левиафана",
+                Price = 75000,
+                PhotoLink = "",
+                Rarity = RarityEnum.Epic,
+                Description = "пояс"
+            });
+            Items.Add(new Item()
+            {
+                Id = 10,
+                Name = "Очки парообразования",
+                Price = 75000,
+                PhotoLink = "",
+                Rarity = RarityEnum.Epic,
+                Description = "голова"
+            });
+            Items.Add(new Item()
+            {
+                Id = 11,
+                Name = "Перчатки огненного чудища",
+                Price = 75000,
+                PhotoLink = "",
+                Rarity = RarityEnum.Epic,
+                Description = "кисти рук"
+            });
+            Items.Add(new Item()
+            {
+                Id = 12,
+                Name = "Пластинчатые поножи опустошения",
+                Price = 150000,
+                PhotoLink = "",
+                Rarity = RarityEnum.Epic,
+                Description = "ноги"
+            });
+            Items.Add(new Item()
+            {
+                Id = 13,
+                Name = "Повязки конструктора",
+                Price = 75000,
+                PhotoLink = "",
+                Rarity = RarityEnum.Epic,
+                Description = "кисти рук"
+            });
+            Items.Add(new Item()
+            {
+                Id = 14,
+                Name = "Подвеска опустошительного пожара",
+                Price = 150000,
+                PhotoLink = "",
+                Rarity = RarityEnum.Epic,
+                Description = "шея"
+            });
+            Items.Add(new Item()
+            {
+                Id = 15,
+                Name = "Раскаленные стяжки Мимирона",
+                Price = 75000,
+                PhotoLink = "",
+                Rarity = RarityEnum.Epic,
+                Description = "наручи"
+            });
+            Items.Add(new Item()
+            {
+                Id = 16,
+                Name = "Руководство по заправке огненного левиафана",
+                Price = 75000,
+                PhotoLink = "",
+                Rarity = RarityEnum.Epic,
+                Description = "левая рука"
+            });
+            Items.Add(new Item()
+            {
+                Id = 17,
+                Name = "Сапоги жаркой развязки",
+                Price = 150000,
+                PhotoLink = "",
+                Rarity = RarityEnum.Epic,
+                Description = "ступни"
+            });
+            Items.Add(new Item()
+            {
+                Id = 18,
+                Name = "Сияющее кольцо регенерации  ",
+                Price = 75000,
+                PhotoLink = "",
+                Rarity = RarityEnum.Epic,
+                Description = "кольцо"
+            });
+            Items.Add(new Item()
+            {
+                Id = 19,
+                Name = "Титановый страж",
+                Price = 75000,
+                PhotoLink = "",
+                Rarity = RarityEnum.Epic,
+                Description = "одноручное"
+            });
+            Items.Add(new Item()
+            {
+                Id = 20,
+                Name = "Иллюзия: Отведение удара",
+                Price = 75000,
+                PhotoLink = "",
+                Rarity = RarityEnum.Epic,
+                Description = "чертеж"
+            });
+            Items.Add(new Item()
+            {
+                Id = 21,
+                Name = "Иллюзия: Вытягивание крови",
+                Price = 75000,
+                PhotoLink = "",
+                Rarity = RarityEnum.Epic,
+                Description = "чертеж"
+            });
+            Items.Add(new Item()
+            {
+                Id = 22,
+                Name = "Потемневший осколок Вал'анира",
+                Price = 175000,
+                PhotoLink = "",
+                Rarity = RarityEnum.Epic,
+                Description = "осколок"
+            });
+            Items.Add(new Item()
+            {
+                Id = 23,
+                Name = "Формула иллюзии: Отведение удара",
+                Price = 75000,
+                PhotoLink = "",
+                Rarity = RarityEnum.Epic,
+                Description = "формула"
+            });
+            Items.Add(new Item()
+            {
+                Id = 24,
+                Name = "Формула иллюзии: Вытягивание крови",
+                Price = 75000,
+                PhotoLink = "",
+                Rarity = RarityEnum.Epic,
+                Description = "формула"
+            });
+            Items.Add(new Item()
+            {
+                Id = 25,
+                Name = "Печать Титанов",
+                Price = 120000,
+                PhotoLink = "",
+                Rarity = RarityEnum.Epic,
+                Description = "улучшение"
+            });
+            Items.Add(new Item()
+            {
+                Id = 26,
+                Name = "Первоэлемент",
+                Price = 25000,
+                PhotoLink = "",
+                Rarity = RarityEnum.Epic,
+                Description = "материал"
             });
 
         }
