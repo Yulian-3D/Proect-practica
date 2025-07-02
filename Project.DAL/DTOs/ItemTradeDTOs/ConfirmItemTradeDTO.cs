2ca9bdb66228dc10fcaf2c8134992a923c28c515
BlackkTurtle <113180669+BlackkTurtle@users.noreply.github.com>
2025-07-02 15:40:00 +0300

    add validation to itemTrades DTOs

--- Project.DAL/DTOs/ItemTradeDTOs/ConfirmItemTradeDTO.cs
+++ Project.DAL/DTOs/ItemTradeDTOs/ConfirmItemTradeDTO.cs
@@ -1,6 +1,7 @@
 ﻿using Project.DAL.Entities;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,9 @@ namespace Project.DAL.DTOs.ItemTradeDTOs
 {
     public class ConfirmItemTradeDTO
     {
+        [Required]
         public int Id { get; set; }
+        [Required]
         public bool IsConfirmed { get; set; }
     }
 }
--- Project.DAL/DTOs/ItemTradeDTOs/CreateItemTradeDTO.cs
+++ Project.DAL/DTOs/ItemTradeDTOs/CreateItemTradeDTO.cs
@@ -1,6 +1,7 @@
 ﻿using Project.DAL.Entities;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,8 +10,12 @@ namespace Project.DAL.DTOs.ItemTradeDTOs
 {
     public class CreateItemTradeDTO
     {
+        [Required]
         public int BuyerUserId { get; set; }
+        [Required]
         public int UserItemId { get; set; }
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "Price must be a positive number.")]
         public int Price { get; set; }
     }
 }
