0661f30c52c3b884955d1b9b3f4ce0ed61a08c92
BlackkTurtle <113180669+BlackkTurtle@users.noreply.github.com>
2025-07-02 23:49:09 +0300

    Fixed GetItemsByUserId endpoint

---
+++ Project.DAL/DTOs/UserItemDTOs/GetUserItemsDTO.cs
@@ -0,0 +1,21 @@
+﻿using Project.DAL.Entities;
+using Project.DAL.Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.DAL.DTOs.UserItemDTOs
+{
+    public class GetUserItemsDTO
+    {
+        public int Id { get; set; }
+        public int ItemId { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public string PhotoLink { get; set; }
+        public RarityEnum Rarity { get; set; }
+        public string Description { get; set; }
+    }
+}
